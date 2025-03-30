111b786739b7174fa98abf21acb146dbec8e6e3d
Katei <kateievans28@gmail.com>
2025-03-30 11:09:15 +0300

    array-map-method

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+	"what does the this keyword mean?",
+	"What is the Constructor OO pattern?",
+	"implementing Blockchain Web API",
+	"The Test Driven Development Workflow",
+	"What is NaN and how Can we Check for it",
+	"What is the difference between stopPropagation and preventDefault?",
+	"Immutable State and Pure Functions",
+	"what is the difference between == and ===?",
+	"what is the difference between event capturing and bubbling?",
+	"what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+let tutorial = tutorials.map((element) =>
+	element
+		.split(" ")
+		.map((element) => element.slice(0, 1)[0].toUpperCase() + element.slice(1))
+		.join(" ")
+);
+	return tutorial
+
+};
